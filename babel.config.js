38ff0736f3f4891584f530819303e00539855cba
Cristina <cristy.blk@gmail.com>
2019-08-24 19:55:47 +0200

    change files for cordova project

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,15 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      modules: false,
-      targets: {
-        browsers: [
-          'Android >= 5',
-          'IOS >= 9.3',
-          'Edge >= 15',
-          'Safari >= 9.1',
-          'Chrome >= 49',
-          'Firefox >= 31',
-          'Samsung >= 5',
-        ],
-      },
-    }],
+  "presets": [
+    ["@babel/preset-env", {
+      "modules": false,
+      "targets": {
+        "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+      }
+    }]
   ],
-  plugins: [
-    'transform-vue-jsx',
+  "plugins": [
+    "transform-vue-jsx",
     // "@babel/plugin-transform-runtime",
-    '@babel/plugin-syntax-dynamic-import',
-  ],
+    "@babel/plugin-syntax-dynamic-import"
+  ]
 };
